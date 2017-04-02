3333a8446e37330a894cdbf65a39ca78034fe1f3
NuclearManD <dylanbrophy@gmail.com>
2017-04-01 22:59:02 -0700

    bugfix
    Proper boot no longer requires an arduino reset

--- NMT_GFX.cpp
+++ NMT_GFX.cpp
@@ -44,7 +44,7 @@ void NMT_GFX::wait_cmd_done(){
 //
 void NMT_GFX::begin(){
   _NTI_GFX_.begin(115200);
-  delay(50);
+  delay(600);
   _NTI_GFX_.write('\r');            // If _NTI_GFX_ card is executing command 51 then terminate it
   for(byte i=0;i<8;i++)           // make sure all commands exit on _NTI_GFX_ card by sending a ton of junk data
     _NTI_GFX_.write('0');           // Reset _NTI_GFX_ card
